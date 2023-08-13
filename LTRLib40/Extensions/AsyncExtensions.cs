df691063f6de67ba7771fc015dd0b0af26c0a02b
LTRData <ol@ltr-data.se>
2023-08-13 23:06:42 +0200

    ValueTask Wait extensions

---
+++ LTRLib40/Extensions/AsyncExtensions.cs
@@ -0,0 +1,56 @@
+﻿#if NET40_OR_GREATER || NETSTANDARD || NETCOREAPP
+
+using System.Threading.Tasks;
+
+namespace LTRLib.Extensions;
+
+public static class AsyncExtensions
+{
+    
+#if NET462_OR_GREATER || NETSTANDARD || NETCOREAPP
+
+    /// <summary>
+    /// Waits for a ValueTask to complete, or throws AggregateException if the ValueTask fails
+    /// </summary>
+    /// <param name="task">ValueTask</param>
+    public static void Wait(this ValueTask task)
+    {
+        if (!task.IsCompletedSuccessfully)
+        {
+            task.AsTask().Wait();
+        }
+    }
+
+    /// <summary>
+    /// Waits for a ValueTask to complete and returns result value, or throws AggregateException if the ValueTask fails
+    /// </summary>
+    /// <param name="task">ValueTask</param>
+    public static void Wait<T>(this ValueTask<T> task)
+    {
+        if (!task.IsCompletedSuccessfully)
+        {
+            task.AsTask().Wait();
+        }
+    }
+
+    /// <summary>
+    /// Waits for a ValueTask to complete and returns result value, or throws AggregateException if the ValueTask fails
+    /// </summary>
+    /// <param name="task">ValueTask</param>
+    public static T WaitForResult<T>(this ValueTask<T> task)
+    {
+        if (task.IsCompletedSuccessfully)
+        {
+            return task.Result;
+        }
+        else
+        {
+            return task.AsTask().Result;
+        }
+    }
+
+#endif
+
+}
+
+#endif
